52ce04bf16481f121f4bc47ce4ea10b3d76a8323
Williamd <williamd4112@hotmail.com>
2016-08-01 20:06:25 +0800

    Chunk done

--- raw_to_pcd.cpp
+++ raw_to_pcd.cpp
@@ -9,6 +9,9 @@ using namespace std;
 struct Point3f
 {
 	float x,y,z;
+	
+	Point3f(float x, float y, float z) : x(x), y(y), z(z) {}
+	Point3f() {}
 };
 
 void writePCDHeader(FILE *fout, int count, int width, int height)
@@ -27,9 +30,9 @@ void writePCDHeader(FILE *fout, int count, int width, int height)
 }
 
 
-static unsigned int fread_chunk(char *buffer, FILE *fp, size_t begin, size_t end)
+static unsigned int fread_chunk(char *buffer, FILE *fp, size_t chunk_size)
 {
-	return fread(buffer, end - begin, 1, fp);
+	return fread(buffer, chunk_size, 1, fp);
 }
 
 static void parse_point3f(vector<Point3f> & point_buffer, char * src, size_t begin)
@@ -41,59 +44,99 @@ static void parse_point3f(vector<Point3f> & point_buffer, char * src, size_t beg
 	float fx = *(float*)x;	
 	float fy = *(float*)y;	
 	float fz = *(float*)z;
-
-	point_buffer.push_back(Point3f{fx, fy, fz});
+	
+	if (fx != 0 || fy != 0 || fz != 0)
+		point_buffer.push_back(Point3f(fx, fy, fz));
 }
 
-static void parse_point3f_chunk(vector<Point3f> & point_buffer, char *src, size_t begin, size_t end)
+static void parse_point3f_chunk(vector<Point3f> & point_buffer, char * src, size_t begin, size_t end)
 {
 	while (begin < end) {
 		parse_point3f(point_buffer, src, begin);
 		begin += sizeof(Point3f);
-	}	
+	}
 }
 
-int main()
+static bool read_raw_pointcloud(
+	const char *filename, 
+	vector<Point3f> & points, 
+	size_t chunk_size, 
+	unsigned int chunk_begin, 
+	unsigned int chunk_end,
+	unsigned int chunk_offset_begin,
+	unsigned int chunk_offset_end
+	)
 {
-	vector<Point3f> points;
-	FILE *fp = fopen("pointcloud.dat", "rb");
-	if(fp != NULL)
-	{
-		printf("Read\n");
-		char tmp[4 * 3];
+	FILE * fin = fopen(filename, "rb");
+	if (fin != NULL) {
+		char *chunk_buffer = new char[chunk_size];
+		memset(chunk_buffer, 0, chunk_size);
+
+		// How many chunk read
+		unsigned int chunk = 0;
 		
-		while(fread(tmp, 12, 1, fp))
-		{
-			char x[4] = {tmp[3], tmp[2], tmp[1], tmp[0]};
-			char y[4] = {tmp[7], tmp[6], tmp[5], tmp[4]};
-			char z[4] = {tmp[11], tmp[10], tmp[9], tmp[8]};
-			
-			float fx = *(float*)x;	
-			float fy = *(float*)y;	
-			float fz = *(float*)z;
-			
-			if(fx != 0 || fy != 0 || fz != 0)
-			{
-				printf("(%f, %f, %f)\n",fx,fy,fz);
-				points.push_back(Point3f{fx, fy, fz});
-			}
+		// Seek to start row
+		fseek(fin, chunk_begin * chunk_size, SEEK_SET);
+		while (chunk < chunk_end && fread_chunk(chunk_buffer, fin, chunk_size)) {
+			parse_point3f_chunk(points, chunk_buffer, chunk_offset_begin, chunk_offset_end);
+			chunk++;
 		}
+		fclose(fin);
+		return true;
 	}
-	
-	
-	FILE *fout = fopen("screenshot.pcd", "w");
+	else {
+		fprintf(stderr, "Read error\n");
+		return false;
+	}
+}
+
+static void write_pcd(const char *filename, vector<Point3f> & points)
+{
+	FILE *fout = fopen(filename, "w");
 	if(fout != NULL)
 	{	
 		writePCDHeader(fout, points.size(), points.size(), 1);
-		for(Point3f p : points)
+		for(int i = 0; i < points.size(); i++)
 		{
+			Point3f & p = points[i];
 			fprintf(fout, "%f %f %f\n", p.x, p.y, p.z);
-
 		}
+		fclose(fout);
 	}
 	else
 	{
 		fprintf(stderr, "Write error");
 	}
+}
+
+int main(int argc, char *argv[])
+{
+	vector<Point3f> points;
+	
+	unsigned int w, h;
+	sscanf(argv[2], "%u", &w);
+	sscanf(argv[3], "%u", &h);
+
+	// (x1, y1) : left top
+	// (x2, y2) : right bottom
+	unsigned int x1, y1, x2, y2;
+	sscanf(argv[4], "%u", &x1);
+	sscanf(argv[5], "%u", &y1);
+	sscanf(argv[6], "%u", &x2);
+	sscanf(argv[7], "%u", &y2);
+	
+	// chunk_size : a row of pointcloud data
+	// chunk_offset_begin/end : col offset
+	// chunk_begin/end : row offset
+	size_t chunk_size = w * sizeof(Point3f);
+	unsigned int chunk_begin = y1;
+	unsigned int chunk_end = y2;
+	unsigned int chunk_offset_begin = x1 * sizeof(Point3f);
+	unsigned int chunk_offset_end = x2 * sizeof(Point3f);
+	
+	read_raw_pointcloud(argv[1], points, chunk_size, chunk_begin, chunk_end, chunk_offset_begin, chunk_offset_end);
+	write_pcd(argv[8], points);
+	
+
 	return 0;
 }
